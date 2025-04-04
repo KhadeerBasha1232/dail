f68806326e05a69d0968c97fff01d9889617b5a7
KhadeerBasha1232 <120262074+KhadeerBasha1232@users.noreply.github.com>
2025-04-04 13:10:29 +0530

    Update job_bot.py

--- job_bot.py
+++ job_bot.py
@@ -145,7 +145,9 @@ async def fetch_linkedin_jobs_one_combination(keyword, location, experience):
     headers = {"User-Agent": "Mozilla/5.0"}
 
     for start in range(0, 51, 25):  # Fetch up to 50 jobs per query
-        url = (f"https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords={keyword}&location={location}&f_TPR=r3600&f_E={experience}&sortBy=DD&start={start}&f_WT=2,3")
+        url = (f"https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?"
+       f"keywords={keyword}&location={location}&f_TPR=r3600&f_E={experience}"
+       f"&sortBy=DD&start={start}&f_WT=2,3&f_JT=F")
         print(f"Fetching jobs from URL: {url}")
         response = requests.get(url, headers=headers)
         if response.status_code == 200:
